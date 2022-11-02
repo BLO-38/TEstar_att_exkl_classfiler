dc357ca08c78f47e779e121f8eb8264b7935bf49
BLO-38 <lars3838@gmail.com>
2022-11-02 23:34:04 +0100

    ytterligare utskrifter

--- src/paket1/EnKlass.java
+++ src/paket1/EnKlass.java
@@ -4,5 +4,8 @@ public class EnKlass {
     public static void main(String[] args) {
         System.out.println("... och en utskrift");
         System.out.println("Och en till faktiskt");
+        System.out.println("Och en till faktiskt");
+        System.out.println("Och en till faktiskt");
+        System.out.println("Och en till faktiskt");
     }
 }
